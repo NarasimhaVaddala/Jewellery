45744b425ba6099bc78942330c2999d2bfedb5ac
Narasimha <vaddalanarasimha@gmail.com>
2024-09-02 21:24:16 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,372 @@
+const bestSellerData = [
+
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Sites-Tanishq-product-catalog/default/images/hi-res/50D2J1FJRAA02_1.jpg",
+        title:"This Jewellery",
+        desc:"This is a crazy Bullshit",
+        price:"199"
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Sites-Tanishq-product-catalog/default/images/hi-res/50D2J1FJRAA02_1.jpg",
+        title:"This Jewellery",
+        desc:"This is a crazy Bullshit",
+        price:"199"
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Sites-Tanishq-product-catalog/default/images/hi-res/502998SCHAAA09_1.jpg",
+        title:"Ear Rings",
+        desc:"This is a crazy Earrings",
+        price:"99"
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Sites-Tanishq-product-catalog/default/images/hi-res/502998SCHAAA09_1.jpg",
+        title:"Ear Rings",
+        desc:"This is a crazy Earrings",
+        price:"99"
+    },
+
+
+]
+
+
+
+// Home page categoty
+
+const categoryData = [
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://cdn.caratlane.com/media/catalog/product/cache/6/image/480x480/9df78eab33525d08d6e5fb8d27136e95/J/S/JS01790-1YP9RS_11_listfront.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.voylla.com/cdn/shop/products/VMJAI45117_MS_360x.jpg?v=1684923056",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.orra.co.in/media/catalog/product/cache/d746469e6b012b04e6ccf325b028e36b/o/t/otn24v30_1_jgxkrx0py5n6cl7p.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/2436/4429/products/AD-294_360x.jpg?v=1677949270",           
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/2436/4429/products/EMS13080036NKER1_360x.jpg?v=1724229999",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwd573ddc8/images/hi-res/50D3FFNKTAA02_2.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwa22ca8a0/images/hi-res/51D4B2YLDAA00_2.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwf35f31d5/images/hi-res/51D4A1YJDAA00_1.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dw21578acd/images/hi-res/51D2A2YXFAA00_1.jpg",
+        text:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwb460f264/images/hi-res/51D4A1YJNAA00_2.jpg",
+        text:"Mangalsutra"
+    },
+]
+
+
+
+
+
+
+// sub categories
+const mangalsutra = [
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://cdn.caratlane.com/media/catalog/product/cache/6/image/480x480/9df78eab33525d08d6e5fb8d27136e95/J/S/JS01790-1YP9RS_11_listfront.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.voylla.com/cdn/shop/products/VMJAI45117_MS_360x.jpg?v=1684923056",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.orra.co.in/media/catalog/product/cache/d746469e6b012b04e6ccf325b028e36b/o/t/otn24v30_1_jgxkrx0py5n6cl7p.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/2436/4429/products/AD-294_360x.jpg?v=1677949270",           
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/2436/4429/products/EMS13080036NKER1_360x.jpg?v=1724229999",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwd573ddc8/images/hi-res/50D3FFNKTAA02_2.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwa22ca8a0/images/hi-res/51D4B2YLDAA00_2.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwf35f31d5/images/hi-res/51D4A1YJDAA00_1.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dw21578acd/images/hi-res/51D2A2YXFAA00_1.jpg",
+        title:"Mangalsutra"
+    },
+    {
+        img:"https://www.tanishq.co.in/on/demandware.static/-/Sites-Tanishq-product-catalog/default/dwb460f264/images/hi-res/51D4A1YJNAA00_2.jpg",
+        title:"Mangalsutra"
+    },
+]
+
+
+const bracelet = [
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Good Bracelet",
+        desc:"skahdkna",
+        price:499,
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Good Bracelet",
+        desc:"skahdkna",
+        price:499,
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Good Bracelet",
+        desc:"skahdkna",
+        price:499,
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Good Bracelet",
+        desc:"skahdkna",
+        price:499,
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Good Bracelet",
+        desc:"skahdkna",
+        price:499,
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Good Bracelet",
+        desc:"skahdkna",
+        price:499,
+    },
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Good Bracelet",
+        desc:"skahdkna",
+        price:499,
+    },
+]
+const bangle = [
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Mangalsutra",
+        desc:"skahdkna",
+        price:499,
+    },
+]
+const necklace = [
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Mangalsutra",
+        desc:"skahdkna",
+        price:499,
+    },
+]
+const rings = [
+    {
+        img:"https://www.tanishq.co.in/dw/image/v2/BKCK_PRD/on/demandware.static/-/Library-Sites-TanishqSharedLibrary/default/dwed9ca8a8/homepage/shopByCategory/diamond-mangalsutra.jpg",
+        title:"Mangalsutra",
+        desc:"skahdkna",
+        price:499,
+    },
+]
+
+
+
+
+function getBestSellers() {
+    let div = document.getElementsByClassName('best-seller-div')[0];
+    
+    bestSellerData.forEach((e) => {
+      let div2 = document.createElement('div')
+      div2.innerHTML = `<div class="my-card">
+                <div class="my-card-img">
+                    <img src=${e.img} alt="xxxx">
+                </div>
+                <div class="my-card-body">
+                    <h3>${e.title}</h3>
+                    <p>${e.desc}</p>
+                    <p>$ ${e.price}</p>
+                </div>
+            </div> `
+            div.appendChild(div2)
+    });
+
+}
+function getCategory() {
+    let div = document.getElementsByClassName('category')[0];
+    
+    categoryData.forEach((e) => {
+      let div2 = document.createElement('div')
+      div2.className = "category-card"  
+      div2.innerHTML = `
+           
+                <div class="my-card-img">
+                    <img src=${e.img}
+                        alt="xxxx">
+                </div>
+                <h6>${e.text}</h6>
+            `
+            div.appendChild(div2)
+    });
+
+}
+function getBracelets() {
+    let div = document.getElementsByClassName('bracelet-div')[0];
+
+    
+    
+    bracelet.forEach((e) => {
+      let div2 = document.createElement('div')
+
+      div2.className = 'my-card col-md-4'
+
+      div2.innerHTML = `
+                <div class="my-card-img">
+                    <img src=${e.img} alt="xxxx">
+                </div>
+                <div class="my-card-body">
+                    <h3>${e.title}</h3>
+                    <p>${e.desc}</p>
+                    <p>$ ${e.price}</p>
+                </div>
+             `
+            div.appendChild(div2)
+    });
+
+}
+function getMangal(){
+    let div = document.getElementsByClassName('mangal-div')[0];
+
+    
+    
+    bangle.forEach((e) => {
+      let div2 = document.createElement('div')
+
+      div2.className = 'my-card col-md-4'
+
+      div2.innerHTML = `
+                <div class="my-card-img">
+                    <img src=${e.img} alt="xxxx">
+                </div>
+                <div class="my-card-body">
+                    <h3>${e.title}</h3>
+                    <p>${e.desc}</p>
+                    <p>$ ${e.price}</p>
+                </div>
+             `
+            div.appendChild(div2)
+    });
+
+}
+function getBangles(){
+    let div = document.getElementsByClassName('bangle-div')[0];
+
+    
+    
+    bangle.forEach((e) => {
+      let div2 = document.createElement('div')
+
+      div2.className = 'my-card col-md-4'
+
+      div2.innerHTML = `
+                <div class="my-card-img">
+                    <img src=${e.img} alt="xxxx">
+                </div>
+                <div class="my-card-body">
+                    <h3>${e.title}</h3>
+                    <p>${e.desc}</p>
+                    <p>$ ${e.price}</p>
+                </div>
+             `
+            div.appendChild(div2)
+    });
+
+}
+function getNecklace(){
+    let div = document.getElementsByClassName('necklace-div')[0];
+
+    
+    
+    necklace.forEach((e) => {
+      let div2 = document.createElement('div')
+
+      div2.className = 'my-card col-md-4'
+
+      div2.innerHTML = `
+                <div class="my-card-img">
+                    <img src=${e.img} alt="xxxx">
+                </div>
+                <div class="my-card-body">
+                    <h3>${e.title}</h3>
+                    <p>${e.desc}</p>
+                    <p>$ ${e.price}</p>
+                </div>
+             `
+            div.appendChild(div2)
+    });
+
+}
+function getRings(){
+    let div = document.getElementsByClassName('rings-div')[0];
+
+    
+    
+    rings.forEach((e) => {
+      let div2 = document.createElement('div')
+
+      div2.className = 'my-card col-md-4'
+
+      div2.innerHTML = `
+                <div class="my-card-img">
+                    <img src=${e.img} alt="xxxx">
+                </div>
+                <div class="my-card-body">
+                    <h3>${e.title}</h3>
+                    <p>${e.desc}</p>
+                    <p>$ ${e.price}</p>
+                </div>
+             `
+            div.appendChild(div2)
+    });
+
+}
